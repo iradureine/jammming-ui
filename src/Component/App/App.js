e2f857ee1654101a27055178910f8c5fa6452911
iradureine <miradukunda601@gmail.com>
2021-11-15 12:52:34 -0800

    hello

---
+++ src/Component/App/App.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/playlist';
+import Deezer from '../../util/deezer';
+
+
+let api;
+if (process.env.NODE_ENV === 'development') {
+  api = 'http://localhost:8005/api/v1/deezer';
+} else {
+  api = 'https://leave-app-sys.herokuapp.com/api/v1/deezer';
+}
+ 
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+  this.state = {
+      searchResults:[],
+       playlistName:'My Playlist',
+      playlistTracks:[]
+    };
+    this.addTrack=this.addTrack.bind(this);
+    this.removeTrack=this.removeTrack.bind(this);
+    this.updatePlaylistName=this.updatePlaylistName.bind(this);
+    this.savePlaylistName=this.savePlaylistName.bind(this);
+    this.search=this.search.bind(this);
+
+  }
+  
+  
+  savePlaylist() {
+    let tracks = this.state.playlistTracks;
+    let token = localStorage.getItem('token');
+    let user_id = localStorage.getItem('user_id');
+    if (tracks.length && this.state.playlistName && token && user_id) {
+      let trackIDs = tracks.map((track) => track.id);
+      Deezer.savePlaylist(
+        this.state.playlistName,
+        trackIDs,
+        token,
+        user_id
+      ).then(() => {
+        this.setState({
+          playlistName: 'New Playlist',
+          playlistTracks: [],
+        });
+        document.getElementById('Playlist-name').value =
+          this.state.playlistName;
+      });
+    }
+  }
+
+  search(term) {
+    Deezer.search(term).then((result) => {
+      this.setState({ searchResults: result });
+    });
+  }
+
+  componentDidMount() {
+    const newAccessToken = window.location.href.match(/token=([^&]*)/);
+    if (newAccessToken) {
+      localStorage.setItem('token', newAccessToken[1]);
+      fetch(`${api}/profile?token=${newAccessToken[1]}`)
+        .then((response) => response.json())
+        .then((json) => {
+          localStorage.setItem('user_id', json.id);
+        });
+    } else {
+      let url = `${api}/auth`;
+      window.location.href = url;
+    }
+  }
+
+  // search(term){
+  //   Spotify.search(term).then(searchResults =>{
+  //     this.setState({searchResults: searchResults})
+  //   })
+
+
+  // }
+  // savePlaylistName(){
+    
+  //   const trackUrls=this.state.playlistTracks.map(track => track.url)
+  // Spotify.savePlayList(this.state.playlistName,trackUrls).then(()=>{
+  //   this.setState({
+  //     playlistName:"new Playlist",
+  //     playlistTracks:[]
+  //   })
+  // })
+  
+  // }
+  updatePlaylistName(name){
+this.setState({playlistName:name})
+  }
+  addTrack(track){
+    let tracks= this.state.playlistTracks;
+    if (tracks.find(savedTrack =>savedTrack.id === track.id)) 
+    {
+      return;
+    }
+    track.push(track);
+    this.setState({playlistTracks:tracks});
+  }
+  removeTrack(track){
+    let tracks=this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack=> currentTrack.id!==track.id);
+    this.setState({playlistTracks:tracks});
+
+  }
+  render(){
+    return(
+      <div>
+         <h1>Ja<span className="highlight">mmm</span>ing</h1>
+               <div className="App">
+               <SearchBar onSearch={this.search}/> 
+                  <div className="App-playlist">
+                    <SearchResults searchResults={this.state.searchResults} 
+                    onAdd={this.addTrack}/>
+
+                    < Playlist playlistName={this.state.playlistName}
+                   playlistTracks={this.state.playlistTracks} 
+                    onRemove={this.removeTrack}
+                    onNameChange={this.updatePlaylistName}
+                    onSave={this.savePlaylistName}/> 
+            
+    </div>
+  </div>
+</div>
+    )
+  }
+}
+export default App;
---
+++ src/util/deezer.js
@@ -0,0 +1,138 @@
+let api;
+if (process.env.NODE_ENV === 'development') {
+  api = 'http://localhost:8005/api/v1/deezer';
+} else {
+  api = 'https://leave-app-sys.herokuapp.com/api/v1/deezer';
+}
+
+const Deezer = {
+  search(term) {
+    return fetch(`${api}/search?term=${term}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((jsonResponse) => {
+        if (!jsonResponse.data) {
+          return [];
+        }
+        return jsonResponse.data.map((track) => ({
+          id: track.id,
+          name: track.title,
+          artist: track.artist.name,
+          album: track.album.title,
+          uri: track.link,
+        }));
+      });
+  },
+  savePlaylist(playlistName, songs, token, userid) {
+    return fetch(
+      `${api}/playlist?userid=${userid}&name=${playlistName}&songs=${songs}&token=${token}`,
+      {
+        method: 'POST',
+      }
+    );
+  },
+};
+
+export default Deezer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const clientId=' 31458e1bac774125bd32d7a1eaa070bb';
+// const redirectUrl='http://localhost:3000'
+// let accessToken;
+
+// const Spotify ={
+// getAccessToken(){
+//     if(accessToken){
+//         return accessToken;
+//     }
+//     const accessTokenMatch = window.location.href.match(/access_token([^&]*)/);
+//     const expiresIn = window.location.href.match(/expires_in([^&]*)/);
+
+
+//     if(accessTokenMatch && expiresIn){
+//         accessToken=accessTokenMatch[1]
+//         const expireIn=Number(expiresIn[1]);
+
+//         window.setTimeout(() => accessToken = '', expiresIn * 1000);
+// window.history.pushState('Access Token', null, '/');
+// return accessToken
+//     }
+//     else{
+       
+//      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUrl}`;
+//    window.location=accessUrl;
+   
+//     }
+
+// },
+// search(term){
+//     const accessToken = Spotify.getAccessToken();
+//     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,{
+//         headers:{
+//             Authorization:`Bearer${accessToken}`
+//         }
+
+//     }).then(response =>{
+//         return response.json();
+//     }).then(jsonrResponse => {
+//         if (!jsonrResponse.tracks){
+//             return [];
+//         }
+//         return jsonrResponse.tracks.items.map(track =>({
+//             id:track.id,
+//             name:track.name,
+//             artist:track.artist[0].name,
+//             album:track.album.name,
+//             url:track.url
+//         }))
+        
+//     })
+// },
+// savePlayList(name,trackUls ){
+// if(!name || trackUls.length){
+//     return;
+
+// }
+// const accessToken = Spotify .getAccessToken();
+// const headers = {Authorization:`Bearer${accessToken}`};
+
+// let userId;
+
+// return fetch('https://api.spotify.com/v1/me', {headers: headers}
+// ).then(response => response.json()
+// ).then(jsonResponse=>{
+//     userId=jsonResponse.id;
+//     return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,{
+//         headers:headers,
+//         method:'POST',
+//         body: JSON.stringify({ name: name})
+//     }).then(response =>response.json()
+//     ).then(jsonResponse =>{
+//           const playlistId= jsonResponse.id;
+//           return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`,
+//           {headers : headers,
+//             method:'POST',
+//             body:JSON.stringify({urls:trackUls})
+        
+//         }
+
+//           )
+
+//     })
+// })
+// }
+
+// }
+// export default Spotify;
